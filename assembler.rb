8f6503302b7525351b5a8d141a3b3bede4cae617
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-05-31 18:02:59 -0700

    Parse a and c commands

--- assembler.rb
+++ assembler.rb
@@ -1,6 +1,58 @@
 class Assembler
   attr_accessor :input, :instructions
 
+  JUMP_CODE = {
+    "JGT" => "001",
+    "JEQ" => "010",
+    "JGE" => "011",
+    "JLT" => "100",
+    "JNE" => "101",
+    "JLE" => "110",
+    "JMP" => "111"
+  }
+
+  DEST_CODE = {
+    "M" => "001",
+    "D" => "010",
+    "MD" => "011",
+    "A" => "100",
+    "AM" => "101",
+    "AD" => "110",
+    "AMD" => "111"
+  }
+
+  COMP_CODE = {
+    # when a = 0
+    "0" => "0101010",
+    "1" => "0111111",
+    "-1" => "0111010",
+    "D" => "0001100",
+    "A" => "0110000",
+    "M" => "1110000",
+    "!D" => "0001101",
+    "!A" => "0110001",
+    "!M" => "1110001",
+    "-D" => "0001111",
+    "-A" => "0110011",
+    "-M" => "1110011",
+    "D+1" => "0011111",
+    "A+1" => "0110111",
+    "M+1" => "1110111",
+    "D-1" => "0001110",
+    "A-1" => "0110010",
+    "M-1" => "1110010",
+    "D+A" => "0000010",
+    "D+M" => "1000010",
+    "D-A" => "0010011",
+    "D-M" => "1010011",
+    "A-D" => "0000111",
+    "M-D" => "1000111",
+    "D&A" => "0000000",
+    "D&M" => "1000000",
+    "D|A" => "0010101",
+    "D|M" => "1010101"
+  }
+
   def initialize input
     @input = sanitize input
     @instructions = []
@@ -11,7 +63,46 @@ class Assembler
     blank = /^\s+$/
     comment = /\/\/\s/
     lines = input.reject { |line| line.match(blank) || line.match(comment) }
-    lines.map { |line| line.delete("\n") }
+    lines.map { |line| line.strip }
+  end
+
+  def parse
+    input.each do |line|
+      a_instr = /@/
+      if a_instr =~ line
+        instruction = a_command line
+      else
+        instruction = c_command line
+      end
+      instructions.push instruction
+    end
+  end
+
+  def a_command line
+    # line should be of format "@symbol" or "@integer"
+    address = line.delete "@"
+    if /\d+/ =~ address
+      register = address.to_i
+    else
+      # address is a symbol; look it up in symbol table
+    end
+    binary = register.to_s(2)
+    sprintf("%016d", binary)
+  end
+
+  def c_command line
+    head = "111"
+    instr, jump = line.split ";"
+    jump_bits = ""
+    if jump
+      jump_bits = JUMP_CODE[jump]
+    else
+      jump_bits = "000"
+    end
+    dest, comp = instr.split "="
+    dest_bits = DEST_CODE[dest]
+    comp_bits = COMP_CODE[comp]
+    head + comp_bits + dest_bits + jump_bits
   end
 end
 
--- assembler_spec.rb
+++ assembler_spec.rb
@@ -1,7 +1,8 @@
 require_relative 'assembler'
 
 describe Assembler do
-  let(:input) { <<input.lines
+  context "with no symbols" do
+    let(:input) { <<input.lines
 // Computes R0 = 2 + 3
 
 @2
@@ -11,12 +12,37 @@ D=D+A
 @0
 M=D
 input
- }
-  let(:assembler) { Assembler.new input }
-  describe "#initialize" do
-    it "sets instructions from input file" do
-      p assembler.input
-      expect(assembler.input.count).to eq 6
+    }
+    let(:assembler) { Assembler.new input }
+
+    describe "#initialize" do
+      it "sets instructions from input file" do
+        expect(assembler.input.count).to eq 6
+      end
+    end
+
+    describe "#a_command" do
+      it "returns binary instruction" do
+        line = "@2"
+        expect(assembler.a_command line).to eq "0000000000000010"
+      end
+    end
+
+    describe "#c_command" do
+      it "returns binary instruction" do
+        line = "D=M"
+        expect(assembler.c_command line).to eq "1111110000010000"
+        line = "M=D+M"
+        expect(assembler.c_command line).to eq "1111000010001000"
+      end
+    end
+
+    describe "#parse" do
+      it "parses instructions into binary" do
+        assembler.parse
+        puts assembler.instructions
+        expect(assembler.instructions).to eq ["0000000000000010", "1110110000010000", "0000000000000011", "1110000010010000", "0000000000000000", "1110001100001000"]
+      end
     end
   end
 end
